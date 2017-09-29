af00cea40c25b07f568d939a3286a040d543a4b4
Nathan Day <adayinukraine@gmail.com>
2017-09-29 16:14:14 +0300

    Allow a user to create a todo

---
+++ spec/features/user_creates_todo_spec.rb
@@ -0,0 +1,11 @@
+feature "User creates todo" do
+  scenario "successfully" do
+    visit root_path
+      
+    click_on "Add a new todo"
+    fill_in "Title", with: "Buy milk"
+    click_on "Create Todo"
+
+    expect(page).to have_css '.todos li', text: 'Buy milk'
+  end
+end
