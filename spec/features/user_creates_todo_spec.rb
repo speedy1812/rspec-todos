a9f5f020db41abd49db9edaa2ac557fa12e5cc0d
Nathan Day <adayinukraine@gmail.com>
2017-10-02 17:33:26 +0300

    Extract todo title to local variable

--- spec/features/user_creates_todo_spec.rb
+++ spec/features/user_creates_todo_spec.rb
@@ -1,9 +1,12 @@
 feature "User creates todo" do
   scenario "successfully" do
+
+    todo_title = "Buy milk"
+
     sign_in
       
-    create_todo "Buy milk"
+    create_todo todo_title
 
-    expect(page).to display_todo 'Buy milk'
+    expect(page).to display_todo todo_title
   end
 end
