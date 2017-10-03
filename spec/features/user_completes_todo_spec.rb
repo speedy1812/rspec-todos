8612d4894877c29836fbce694056160c9ba44037
Nathan Day <adayinukraine@gmail.com>
2017-10-03 10:23:49 +0300

    Allow todos to be marked as incomplete

--- spec/features/user_completes_todo_spec.rb
+++ spec/features/user_completes_todo_spec.rb
@@ -4,8 +4,6 @@ feature "User completes todo" do
       
     create_todo "Buy milk"
 
-    expect(page).to have_css '.todos li', text: 'Buy milk'
-
     click_on "Mark complete"
 
     expect(page).to display_completed_todo 'Buy milk'
